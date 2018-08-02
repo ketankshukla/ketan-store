4bfcada0c7fe7311407ee4f8c3d129b8ed57e820
Ketan Shukla <ketankshukla@gmail.com>
2018-08-02 13:57:48 -0700

    adopted project to create-react-app format and added resources

---
+++ src/components/StorePicker.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class StorePicker extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Ketan's Store Picker</h1>
+      </div>
+    )
+  }
+}
+
+export default StorePicker
