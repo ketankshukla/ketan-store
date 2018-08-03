5efe9007a59038313eb8fad7f703cda17993da9d
Ketan Shukla <ketankshukla@gmail.com>
2018-08-03 14:56:20 -0700

    added Router and NotFound components

---
+++ src/components/NotFound.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class NotFound extends Component {
+  render() {
+    return (
+      <div>
+        Sorry Page not found
+      </div>
+    )
+  }
+}
+
+export default NotFound
